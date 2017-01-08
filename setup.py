c601ce603ab947b1c8f2aa6c5fbd27191d621ef8
virtualguywithabowtie <virtualguywithabowtie@users.noreply.github.com>
2017-01-08 17:11:46 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ def install_driver():
 
 install_driver()
 setup(name="Orientation",
-      version="2.0.2",
+      version="2.0.3",
       description="Auto Orientation Indicator for Ubuntu on Surface",
       url='https://github.com/vguywithabowtie/orientation',
       author='VirtualGuywithaBowTie',
